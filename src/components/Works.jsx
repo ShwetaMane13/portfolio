f902a33a7e84b67c396d534783db651796c9e0fc
Shweta Mane <shwetamane1397@gmail.com>
2025-08-21 16:16:57 +0530

    added content and styling to work section

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -24,43 +24,56 @@ export const Works = () => {
   const scrollRef = useRef(null);
   const { scrollYProgress } = useScroll({
     target: scrollRef,
-    offset: ["end center", "end start"]
+    offset: ["end center", "end start"],
   });
 
   const xTransform = useTransform(scrollYProgress, [0, 0.1, 1], [0, 0, -1400]);
 
   return (
     <div className="md:h-screen flex flex-col bg-[#f5f5f0] overflow-hidden">
-      <div className="text-4xl md:text-5xl text-center py-8 md:px-50 md:py-12">Selected Works</div>
+      <div className="text-4xl md:text-5xl text-center py-8 md:px-50 md:py-12">
+        Selected Works
+      </div>
       <div ref={scrollRef} className="hidden md:block">
         <motion.div className="z-6 relative" style={{ translateX: xTransform }}>
           <div className="ml-120 m-8 p-2 flex gap-8 absolute">
             {projects.map((project) => (
-              <div className="bg-white w-[600px] px-4 py-4 rounded-2xl">
+              <div className="bg-white h-[500px] w-[600px] px-4 py-4 rounded-2xl">
                 <img
                   className="h-2/3 w-full rounded-2xl"
                   src={project.img}
                   alt=""
                 />
+                <p className="text-3xl p-4">{project.title}</p>
+                <p className="text-2xl pl-4">{project.description}</p>
+                <div className="flex justify-between p-4 mt-4">
+                  <section>source</section>
+                  <section>link</section>
+                </div>
               </div>
             ))}
           </div>
         </motion.div>
       </div>
 
-
       <div className="md:hidden h-auto">
-          <div className="p-8 flex flex-col gap-8">
-            {projects.map((project) => (
-              <div className="bg-white md:w-[600px] px-4 py-4 rounded-2xl">
-                <img
-                  className="md:h-2/3 w-full rounded-2xl"
-                  src={project.img}
-                  alt=""
-                />
+        <div className="p-8 flex flex-col gap-8">
+          {projects.map((project) => (
+            <div className="bg-white h-[300px] md:w-[600px] px-4 py-4 rounded-2xl">
+              <img
+                className="h-2/3 w-full rounded-2xl"
+                src={project.img}
+                alt=""
+              />
+              <p className="text-2xl p-2">{project.title}</p>
+              <p className="text-base pl-2">{project.description}</p>
+              <div className="flex justify-between px-2">
+                <section className="text-sm">source</section>
+                <section className="text-sm">link</section>
               </div>
-            ))}
-          </div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
