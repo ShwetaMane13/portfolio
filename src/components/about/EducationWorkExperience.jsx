c982d7acd2b2e452bc5c476ca617fdf7f24818ac
Shweta Mane <shwetamane1397@gmail.com>
2025-08-21 16:42:00 +0530

    padding changes

--- src/components/about/EducationWorkExperience.jsx
+++ src/components/about/EducationWorkExperience.jsx
@@ -7,7 +7,7 @@ export const EducationWorkExperience = () => {
   return (
     <div
       ref={ref}
-      className="h-auto lg:h-120 flex flex-col gap-6 lg:flex-row px-12 lg:px-50 items-start lg:items-center bg-[#f5f5f0]"
+      className="h-auto pb-8 lg:pb-0 lg:h-120 flex flex-col gap-6 lg:flex-row px-12 lg:px-50 items-start lg:items-center bg-[#f5f5f0]"
     >
       <div className="flex-1 flex flex-col">
         <section className="font-inter mt-8 lg:mt-0 text-3xl lg:text-[40px]">
